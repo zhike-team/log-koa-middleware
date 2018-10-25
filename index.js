5ec1ec2a340fb49fee9ac69ec4fe29d3c2b0f40a
renxing <renxing@innobuddy.com>
2018-10-25 16:48:21 +0800

    fix reqId generate

--- index.js
+++ index.js
@@ -12,27 +12,29 @@ function logger (opts) {
     responseBodyBlackList:[]
   }
 
+  // 柯里化，对console.log添加reqId
+  const originalLogger = console.log 
+
   // 初始化配置
   if (typeof opts === 'object') {
     defaultOptions = Object.assign(defaultOptions, opts)
   }
 
-  // 初始化响应id
-  if (typeof opts === 'object' && opts.reqId) {
-    reqId = opts.reqId
-  } else {
-    reqId = uuidv1()
-  }
-
   return async function (ctx, next) {
+
+    // 初始化响应id
+    if (typeof opts === 'object' && opts.reqId) {
+      reqId = ctx[opts.reqId]
+    } else {
+      reqId = uuidv1()
+    }
+
     const startTime = new Date()
 
-    // 柯里化，对console.log添加reqId
-    const orginalLogger = console.log 
-    console.log = console.log.bind(console, `${reqId}: `)
+    console.log = originalLogger.bind(console, `${reqId}: `)
 
-    orginalLogger(`----> ${reqId}`)
-    orginalLogger(ctx.method, ctx.originalUrl)
+    originalLogger(`----> ${reqId}`)
+    originalLogger(ctx.method, ctx.originalUrl)
 
     // 打印requestHeaders
     const headers = ctx.headers
@@ -40,14 +42,14 @@ function logger (opts) {
     if (Array.isArray(requestHeaders) && requestHeaders.length > 0) {
       requestHeaders.forEach(item => {
         if (headers[item.toLowerCase()]) {
-          orginalLogger(`${item}: ${headers[item]}`)
+          originalLogger(`${item}: ${headers[item]}`)
         }
       })
     }
 
     // 打印requestBody的配置
     if (ctx.request.body) {
-      orginalLogger(ctx.request.body)
+      originalLogger(ctx.request.body)
     }
 
     // bling-bling
@@ -57,14 +59,14 @@ function logger (opts) {
     // 打印完整url及响应时间
     const endTime = new Date()
 
-    orginalLogger(`<---- ${reqId} ${ctx.status} ${endTime - startTime} ms`)
+    originalLogger(`<---- ${reqId} ${ctx.status} ${endTime - startTime} ms`)
     // 打印responseHeaders
     const resHeaders = ctx.response.headers
     const responseHeaders = defaultOptions.responseHeaders
     if (Array.isArray(responseHeaders) && responseHeaders.length > 0) {
       responseHeaders.forEach(item => {
         if (resHeaders[item.toLowerCase()]) {
-          orginalLogger(`${item}: ${resHeaders[item]}`)
+          originalLogger(`${item}: ${resHeaders[item]}`)
         }
       })
     }
@@ -82,13 +84,13 @@ function logger (opts) {
       if (Array.isArray(responseBodyWhiteList) && responseBodyWhiteList.length>0) {
         for (let path of responseBodyWhiteList) {
           if (path === ctx.path || (path instanceof RegExp && path.test(ctx.path))) {
-            orginalLogger(logBody)
+            originalLogger(logBody)
           } 
         }
       } else if (Array.isArray(responseBodyBlackList) && responseBodyBlackList.length>0) {
         for (let path of responseBodyBlackList) {
           if (path !== ctx.path && (path instanceof RegExp && !path.test(ctx.path))) {
-            orginalLogger(logBody)
+            originalLogger(logBody)
           } 
         }
       }
